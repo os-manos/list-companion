873ee0f64490bbc1b0db46d3d7d448bda64f0622
Gabriel Silvino <gabriel.siv720@gmail.com>
2025-03-20 16:56:47 -0300

    fix: button adjustments

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,8 +1,6 @@
-import * as React from "react"
-import { Slot } from "@radix-ui/react-slot"
-import { cva, type VariantProps } from "class-variance-authority"
-
-import { cn } from "@/lib/utils"
+import * as React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
@@ -19,7 +17,6 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
-        link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
@@ -33,19 +30,15 @@ const buttonVariants = cva(
       size: "default",
     },
   }
-)
+);
 
 function Button({
   className,
   variant,
   size,
-  asChild = false,
   ...props
-}: React.ComponentProps<"button"> &
-  VariantProps<typeof buttonVariants> & {
-    asChild?: boolean
-  }) {
-  const Comp = asChild ? Slot : "button"
+}: React.ComponentProps<"button"> & VariantProps<typeof buttonVariants>) {
+  const Comp = "button";
 
   return (
     <Comp
@@ -53,7 +46,7 @@ function Button({
       className={cn(buttonVariants({ variant, size, className }))}
       {...props}
     />
-  )
+  );
 }
 
-export { Button, buttonVariants }
+export { Button, buttonVariants };
---
+++ src/stories/Button.stories.tsx
@@ -0,0 +1,35 @@
+import type { Meta } from "@storybook/react";
+import { fn } from "@storybook/test";
+
+import { Button } from "@/components/ui/button";
+
+const meta = {
+  title: "ShadCN Components/Button",
+  component: Button,
+  parameters: {
+    layout: "centered",
+  },
+  argTypes: {
+    disabled: { control: "boolean" },
+    size: {
+      control: { type: "select" },
+      options: ["default", "sm", "lg", "icon"],
+    },
+    variant: {
+      control: { type: "select" },
+      options: ["default", "destructive", "outline", "secondary", "ghost"],
+    },
+  },
+  args: {
+    onClick: fn(),
+    size: "default",
+    variant: "default",
+    disabled: false,
+  },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+
+export const Default = ({ ...props }: React.ComponentProps<typeof Button>) => (
+  <Button {...props}>Click me!</Button>
+);
