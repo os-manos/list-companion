ad487167ca2a3d4e729a79ec2701a54da5342223
Gabriel Silvino <gabriel.siv720@gmail.com>
2025-03-20 17:20:05 -0300

    feat: trash icon

---
+++ src/stories/TrashIcon.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Trash2 } from "lucide-react";
+
+const meta = {
+  title: "Lucid Icons/Trash",
+  component: Trash2,
+  parameters: {
+    layout: "centered",
+  },
+  argTypes: { size: { control: "number" }, color: { control: "color" } },
+  args: { size: 36, color: "#FFFF" },
+} satisfies Meta<typeof Trash2>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
