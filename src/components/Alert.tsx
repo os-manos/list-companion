426c15c1627855abcb9e19d64413108d0e3a232c
Gabriel Silvino <gabriel.siv720@gmail.com>
2025-03-22 12:54:21 -0300

    feat: alert from shadcn

---
+++ src/components/Alert.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import type { SVGProps } from "react";
+
+const ErrorIcon = ({ ...props }: SVGProps<SVGSVGElement>) => {
+  return <div></div>;
+};
+
+export default ErrorIcon;
