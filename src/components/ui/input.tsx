2a3d48f86926ca55765f9ad1ac9df3f94b73eb9a
Gabriel Silvino <gabriel.siv720@gmail.com>
2025-04-08 19:44:13 -0300

    fix: root css clean

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -12,11 +12,7 @@ function InputRoot({
       type={type}
       data-slot="input"
       className={cn(
-        "file:text-foreground text-content-primary placeholder:text-content-tertiary rounded-lg selection:bg-primary selection:text-primary-foreground border-input flex h-9 w-full min-w-0 border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-        "focus-visible:border-brand focus-visible:placeholder:text-content-primary focus-visible:transition-colors",
-        "hover:border-border-hover hover:transition-colors",
-        "selection:bg-content-tertiary border-2 border-border-primary",
-        "aria-invalid:border-destructive aria-invalid:ring-destructive/20",
+        "transition-all duration-200 ease-in-out md:file:h-7 md:file:text-sm aria-invalid:border-destructive aria-invalid:ring-destructive/20 selection:bg-content-tertiary border-2 border-border-primary hover:border-border-hover hover:transition-colors focus-visible:border-brand focus-visible:placeholder:text-content-primary focus-visible:transition-colors file:text-foreground text-content-primary placeholder:text-content-tertiary rounded-lg selection:text-primary-foreground flex h-11 w-full min-w-0 bg-transparent px-3 py-2.5 text-base shadow-xs outline-none file:inline-flex file:h-6 file:border-0 file:bg-transparent file:text-xs file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-base leading-normal",
         className
       )}
       {...props}
