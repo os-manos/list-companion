76e4fe8eadf580e1e571eceb19b040c99083eec2
Gabriel Silvino <gabriel.siv720@gmail.com>
2025-04-08 18:19:46 -0300

    fix: design system css variables on shadcn input and pipe removotion

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -8,18 +8,11 @@ function Input({ className, type, ...props }: React.ComponentProps<"input">) {
       type={type}
       data-slot="input"
       className={cn(
-        "file:text-foreground text-content-primary placeholder:text-content-tertiary",
-        "selection:bg-border-primary dark:bg-input/30 border-2 border-border-primary",
-        "flex w-full min-w-0 rounded-lg bg-transparent outline-none",
-        "h-11 px-3 py-2.5 text-sm leading-normal",
-        "file:inline-flex file:h-6 file:border-0 file:bg-transparent file:text-xs file:font-medium",
+        "h-11 px-3 py-2.5 leading-normal",
+        "file:h-6 file:text-xs",
         "md:h-12 md:px-4 md:py-3 md:text-base md:file:h-7 md:file:text-sm",
         "transition-all duration-200 ease-in-out",
-        "hover:border-border-hover hover:transition-colors",
-        "focus-visible:border-brand focus-visible:placeholder:text-content-primary focus-visible:transition-colors",
         "disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50",
-        "aria-invalid:border-destructive aria-invalid:ring-destructive/20",
-        "dark:aria-invalid:ring-destructive/40",
         className
       )}
       {...props}
