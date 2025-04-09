e0d818669d960c1a84dd62aa49f859bdec086216
Gabriel Silvino <gabriel.siv720@gmail.com>
2025-04-09 13:49:04 -0300

    fix: input refactor

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 import { InputRoot } from "./ui/input";
 import { ShowIcon } from "./icons/Show";
 import { HideIcon } from "./icons/Hide";
+import { UserIcon } from "./icons/User";
+import { SvgProps } from "@/lib/Props";
+import { cn } from "@/lib/utils";
 
 type AllowedInputs = "email" | "password" | "text";
 
@@ -9,58 +12,101 @@ type Props = React.ComponentProps<"input"> & {
   type?: AllowedInputs;
 };
 
-const PasswordInput: React.FC<Props> = ({ className, ...props }) => {
+type PasswordProps = {
+  shown: React.FC<SvgProps>;
+  hide: React.FC<SvgProps>;
+};
+
+type IconProps = {
+  [K in AllowedInputs]: K extends "password"
+    ? PasswordProps
+    : React.FC<SvgProps>;
+};
+
+const Icons: Partial<IconProps> = {
+  email: UserIcon,
+  password: {
+    hide: HideIcon,
+    shown: ShowIcon,
+  },
+};
+
+const PasswordInput: React.FC<Props> = ({ disabled, ...props }) => {
   const [showPassword, setShowPassword] = useState(false);
-  const Icon = showPassword ? HideIcon : ShowIcon;
+  const Icon = showPassword ? Icons.password!.hide : Icons.password!.shown;
 
   return (
     <div className="relative flex items-center">
       <InputRoot
-        type={showPassword ? "text" : "password"}
+        disabled={disabled}
         data-slot="input"
-        className={`peer ${className}`}
+        className={"peer"}
         {...props}
+        type={showPassword ? "text" : "password"}
       />
       <Icon
-        className={`${
-          props.disabled && "pointer-events-none cursor-not-allowed opacity-50"
-        } peer-focus:fill-brand fill-border-primary peer-hover:fill-border-hover peer-hover:transition-colors peer-focus-visible:fill-brand absolute right-3 h-5 w-5 hover:cursor-pointer`}
+        className={cn(
+          "peer-focus:fill-brand fill-border-primary",
+          "peer-hover:fill-border-hover peer-hover:transition-colors",
+          "peer-focus-visible:fill-brand",
+          "absolute right-3 h-5 w-5 hover:cursor-pointer",
+          {
+            "pointer-events-none cursor-not-allowed opacity-50": disabled,
+          }
+        )}
         onClick={() => setShowPassword(!showPassword)}
-        aria-disabled={props.disabled}
+        aria-disabled={disabled}
       />
     </div>
   );
 };
 
-const EmailInput: React.FC<Props> = ({ className, ...props }) => {
+const IconInput: React.FC<Props> = ({ disabled, type, ...props }) => {
+  const Icon = type && typeof Icons[type] === "function" && Icons[type];
+
   return (
-    <InputRoot
-      type={"email"}
-      data-slot="input"
-      className={className}
-      {...props}
-    />
+    <div className="relative flex items-center">
+      <InputRoot
+        disabled={disabled}
+        type={type}
+        data-slot="input"
+        className={"peer"}
+        {...props}
+      />
+      {Icon && (
+        <Icon
+          className={cn(
+            "peer-focus:fill-brand fill-border-primary",
+            "peer-hover:fill-border-hover peer-hover:transition-colors",
+            "peer-focus-visible:fill-brand",
+            "absolute right-3 h-5 w-5 hover:cursor-pointer",
+            {
+              "pointer-events-none cursor-not-allowed opacity-50": disabled,
+            }
+          )}
+          aria-disabled={disabled}
+        />
+      )}
+    </div>
   );
 };
 
-const TextInput: React.FC<Props> = ({ className, ...props }) => {
+const DefaultInput: React.FC<Props> = ({ disabled, ...props }) => {
   return (
-    <InputRoot
-      type={"text"}
-      data-slot="input"
-      className={className}
-      {...props}
-    />
+    <InputRoot disabled={disabled} type="text" data-slot="input" {...props} />
   );
 };
 
-const inputComponents: Record<AllowedInputs, React.FC<Props>> = {
-  email: EmailInput,
-  text: TextInput,
-  password: PasswordInput,
-};
+export const Input: React.FC<Props> = ({ type, disabled, ...props }) => {
+  const inputType = type ?? "text";
 
-export const Input: React.FC<Props> = ({ type, ...props }) => {
-  const InputComponent = type ? inputComponents[type] : inputComponents["text"];
-  return <InputComponent {...props} />;
+  if (inputType === "text") {
+    return <DefaultInput disabled={disabled} {...props} />;
+  }
+  if (inputType === "password") {
+    return <PasswordInput disabled={disabled} {...props} />;
+  }
+  if (typeof Icons[inputType] === "function") {
+    return <IconInput type={inputType} disabled={disabled} {...props} />;
+  }
 };
